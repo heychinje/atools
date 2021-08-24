c9e0594302c00ff1a660edecf43d4a6a0b5c3506
Jason (Zheng, Haiqiang) <hqzheng@telenavsoftware.com>
2021-08-24 17:04:32 +0800

    08/24: change files line separater

--- app/src/main/java/com/heychinje/atools/MainActivity.kt
+++ app/src/main/java/com/heychinje/atools/MainActivity.kt
@@ -1,1 +1,11 @@
-package com.heychinje.atoolsimport androidx.appcompat.app.AppCompatActivityimport android.os.Bundleclass MainActivity : AppCompatActivity() {    override fun onCreate(savedInstanceState: Bundle?) {        super.onCreate(savedInstanceState)        setContentView(R.layout.activity_main)    }}
+package com.heychinje.atools
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
